70784ca8b4d74c02609748c277ba072df463094a
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-10-08 18:50:24 +0100

    create new react app and add routes

---
+++ client/habit-app/src/pages/HabitsPage/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HabitsPage() {
+    return(
+        <h2>HabitsPage</h2>
+    )
+}
+
+export default HabitsPage;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require("./server") // bring in server from server.js
 
-const port = process.env.PORT || 3000; // set port to environment port, if not available run on 3000
+const port = process.env.PORT || 3001; // set port to environment port, if not available run on 3001
 
 app.listen(port, (console.log(`Express departing from port ${port}`))) // runs server
